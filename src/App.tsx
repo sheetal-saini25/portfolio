f8d7ffdd0536c79e7333bb06483803bb3fe04e66
Your Name <you@example.com>
2025-07-12 13:46:51 +0530

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,7 @@ function App() {
         <About />
         <Education />
         <Experience />
-       
+        <Projects />
         <Skills />
         <Certifications />
         <Contact />
