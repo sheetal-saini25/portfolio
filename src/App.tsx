8d7ad98db8e9fd6eaec4f852f83a4bdb07629c08
Your Name <you@example.com>
2025-07-12 23:12:21 +0530

    p12

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import About from './components/About';
 import Education from './components/Education';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Certifications from './components/Certifications';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -86,7 +85,6 @@ function App() {
         <Education />
         <Projects />
         <Skills />
-        <Certifications />
         <Contact />
         <Footer />
       </div>
